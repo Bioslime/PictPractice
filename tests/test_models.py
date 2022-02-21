3e82f8a79d83a76c1c6100978623f81a943e7a75
Bio.slime.man@gmail.com <ok.kabu.you@gmail.com>
2022-02-22 00:55:32 +0900

    'test'

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,14 +1,56 @@
-from cgi import test
 from django.test import TestCase
 from pictdata.models import PictDataModel
 from accounts.models import CustomUser
+from django.test import client
+from PIL import Image
+from io import BytesIO
 
-class PictureTestCase(TestCase):
-    # def setUp(self):
+class CustomUserTestCase(TestCase):
+    def setUp(self):
+        CustomUser.objects.create(username='test', password='testpass1', email='testuser@test.com')
 
-    def test_user_check(self):
-        CustomUser.objects.create(username='test', password='testpass1')
+    def test_check_data_one(self):
+        data = CustomUser.objects.all()
+        self.assertEqual(data.count(), 1)
+        
+    def test_customuser_check(self):
         test = CustomUser.objects.get(username='test')
 
         self.assertEqual(test.username, 'test')
         self.assertEqual(test.password, 'testpass1')
+        self.assertEqual(test.email, 'testuser@test.com')
+        self.assertFalse(test.is_staff)
+        self.assertTrue(test.is_active)
+
+
+class PictureTestCase(TestCase):
+    def setUp(self):
+        CustomUser.objects.create(username='defalut', password='defalut001', email='defalut@def.com')
+
+    def test_zero_check(self):
+        tmp = PictDataModel.objects.all()
+        self.assertEqual(tmp.count(), 0)
+
+    def test_picture_check(self):
+        user=CustomUser.objects.get(username='defalut')
+        dummy_adress = r'C.\media\test_image\test.png'
+        PictDataModel.objects.create(user=user, title='dummy', picture=dummy_adress)
+        getData = PictDataModel.objects.get(title='dummy')
+        
+        self.assertEqual(getData.user, user)
+        self.assertEqual(getData.title, 'dummy')
+        self.assertEqual(getData.picture, dummy_adress)
+
+    def test_anotherPict(self):
+        user=CustomUser.objects.get(username='defalut')
+
+        dummy_adress1 = r'.\media\test_image\test1.png'
+        dummy_adress2 = r'.\media\test_image\test2.png'
+
+        PictDataModel.objects.create(user=user, title='dummy1', picture=dummy_adress1)
+        tmp1 = PictDataModel.objects.get(title='dummy1')
+
+        PictDataModel.objects.create(user=user, title='dummy2', picture=dummy_adress2, anotherPict=tmp1)
+        tmp2 = PictDataModel.objects.get(title='dummy2')
+
+        self.assertEqual(tmp2.anotherPict, tmp1)
