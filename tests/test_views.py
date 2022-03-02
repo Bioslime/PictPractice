7c2441429a673750b87f0b3fe0c098054b17f703
Bio.slime.man@gmail.com <ok.kabu.you@gmail.com>
2022-03-02 15:47:34 +0900

    'test_comments'

--- tests/test_views.py
+++ tests/test_views.py
@@ -27,6 +27,17 @@ class UserTestCase(TestCase):
         res = self.client.login(username='dummy', password='dummypass1')
         self.assertFalse(res)
 
+    def test_post_no_username(self):
+        password = 'testpass001'
+        res = self.client.post(self.url, data={'password': password, 'password2': password}, formt='json')
+        self.assertEqual(res.status_code, 400)
+
+    def test_post_no_password(self):
+        username = 'test'
+        res = self.client.post(self.url, data={'username': username}, formt='json')
+        self.assertEqual(res.status_code, 400)
+
+
 
 class PictureTestCase(TestCase):
     def setUp(self):
@@ -128,4 +139,133 @@ class PictureTestCase(TestCase):
 
 class RandomModelTestCase(TestCase):
     def setUp(self):
-        pass
+        self.url = '/api/questions/'
+        self.client = APIClient()
+
+        self.client.post('/api/user/', data={'username': 'defalut', 'password': 'defalutpass1', 'password2': 'defalutpass1', 'email': 'defalut@test.com'}, formt='json')
+        res = self.client.get('/api/user/', data={'username': 'defalut', 'password': 'defalutpass1'}, format='json')
+        self.token = 'Bearer ' + res.data[0]['token']
+
+    def test_question_get_zero_check(self):
+        self.client.credentials(HTTP_AUTHORIZATION=self.token)
+        res = self.client.get(self.url)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(len(res.data), 0)
+
+    def test_post_no_token(self):
+        text = 'test_question'
+        data = {'question': text}
+        res = self.client.post(self.url, data=data)
+        self.assertEqual(res.status_code, 401)
+
+    def test_post_with_token(self):
+        text = 'test_question'
+        data = {'question': text}
+        self.client.credentials(HTTP_AUTHORIZATION=self.token)
+        res = self.client.post(self.url, data=data)
+        self.assertEqual(res.status_code, 201)
+
+        res = self.client.get(self.url)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(len(res.data), 1)
+        self.assertEqual(res.data[0]['question'], text)
+
+    def test_post_no_question(self):
+        data = {}
+        self.client.credentials(HTTP_AUTHORIZATION=self.token)
+        res = self.client.post(self.url, data=data)
+        self.assertEqual(res.status_code, 400)
+
+
+
+class CommentTestCase(TestCase):
+    def setUp(self):
+        self.url = '/api/comment/'
+        self.client = APIClient()
+
+        self.client.post('/api/user/', data={'username': 'defalut', 'password': 'defalutpass1', 'password2': 'defalutpass1', 'email': 'defalut@test.com'}, formt='json')
+        res = self.client.get('/api/user/', data={'username': 'defalut', 'password': 'defalutpass1'}, format='json')
+        self.token = 'Bearer ' + res.data[0]['token']
+        self.user_uid = res.data[0]['id']
+
+        picture_url = '/api/picture/'
+        picture_adress = r'.\media\test_image\test.png'
+        with open(picture_adress, 'rb') as f:
+            img = f.read()
+        data = {'user_uid': self.user_uid, 'picture': SimpleUploadedFile('test.png', img, content_type='image/png'), 'title': 'test'}
+        self.client.credentials(HTTP_AUTHORIZATION=self.token)
+        res = self.client.post(picture_url, data=data, format='multipart')
+        self.picture_uid = res.data['id']
+        self.client.credentials(HTTP_AUTHORIZATION='')
+
+    def test_get_zero_check(self):
+        self.client.credentials(HTTP_AUTHORIZATION=self.token)
+        res = self.client.get(self.url)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(len(res.data), 0)
+
+    def test_post_with_token(self):
+        comment = 'test_comment'
+        goodbad = True
+        data = {'comment':comment, 'goodbad': goodbad, 'picture_id': self.picture_uid, 'user_uid': self.user_uid }
+        self.client.credentials(HTTP_AUTHORIZATION=self.token)
+        res = self.client.post(self.url, data=data)
+        self.assertEqual(res.status_code, 201)
+        self.assertEqual(res.data['comment'], comment)
+
+    def test_post_no_token(self):
+        comment = 'test_comment'
+        goodbad = True
+        data = {'comment':comment, 'goodbad': goodbad, 'picture_id': self.picture_uid, 'user_uid': self.user_uid }
+        res = self.client.post(self.url, data=data)
+        self.assertEqual(res.status_code, 401)
+
+    def test_post_no_user(self):
+        comment = 'test_comment'
+        goodbad = True
+        data = {'comment':comment, 'goodbad': goodbad, 'picture_id': self.picture_uid}
+        self.client.credentials(HTTP_AUTHORIZATION=self.token)
+        res = self.client.post(self.url, data=data)
+        self.assertEqual(res.status_code, 400)
+
+    def test_post_no_picture(self):
+        comment = 'test_comment'
+        goodbad = True
+        data = {'comment':comment, 'goodbad': goodbad, 'user_uid': self.user_uid }
+        self.client.credentials(HTTP_AUTHORIZATION=self.token)
+        res = self.client.post(self.url, data=data)
+        self.assertEqual(res.status_code, 400)
+
+    def test_post_no_comment(self):
+        goodbad = True
+        data = {'goodbad': goodbad, 'picture_id': self.picture_uid, 'user_uid': self.user_uid }
+        self.client.credentials(HTTP_AUTHORIZATION=self.token)
+        res = self.client.post(self.url, data=data)
+        self.assertEqual(res.status_code, 400)
+
+    def test_post_no_goodbad(self):
+        comment = 'test_comment'
+        data = {'comment':comment, 'picture_id': self.picture_uid, 'user_uid': self.user_uid }
+        self.client.credentials(HTTP_AUTHORIZATION=self.token)
+        res = self.client.post(self.url, data=data)
+        self.assertEqual(res.status_code, 400)
+
+    def test_get(self):
+        comment = 'test_comment'
+        goodbad = True
+        data = {'comment':comment, 'goodbad': goodbad, 'picture_id': self.picture_uid, 'user_uid': self.user_uid }
+        self.client.credentials(HTTP_AUTHORIZATION=self.token)
+        res = self.client.post(self.url, data=data)
+        res = self.client.get(self.url + res.data['id'] + '/')
+        self.assertEqual(res.data['comment'], comment)
+
+    def test_post_with_another_comment(self):
+        comment = 'test_comment'
+        goodbad = True
+        data = {'comment':comment, 'goodbad': goodbad, 'picture_id': self.picture_uid, 'user_uid': self.user_uid }
+        self.client.credentials(HTTP_AUTHORIZATION=self.token)
+        res = self.client.post(self.url, data=data)
+        another_comment = res.data['id']
+        data = {'comment':comment, 'goodbad': goodbad, 'picture_id': self.picture_uid, 'user_uid': self.user_uid, 'another_comment':another_comment }
+        res = self.client.post(self.url, data=data)
+        self.assertEqual(str(res.data['another_comment']), another_comment)
